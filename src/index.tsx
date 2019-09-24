43515173a5850ab6f44e7a1f8500d4d36eda86e2
Nick Nance <nance.nick@gmail.com>
2019-09-23 21:43:56 -0700

    route links

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,17 @@ import * as serviceWorker from "./serviceWorker";
 const routing = (
   <Router>
     <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/users">Users</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
       <Route exact path="/" component={App} />
       <Route path="/users" component={Users} />
       <Route path="/contact" component={Contact} />
