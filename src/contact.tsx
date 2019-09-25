ac0d06e2998208cb4037c481900a16c79a1210a1
Nick Nance <nance.nick@gmail.com>
2019-09-24 21:52:01 -0700

    fix lint errors

--- src/contact.tsx
+++ src/contact.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { withRouter } from "react-router-dom";
 
 const Contact = withRouter(({ history }) => {
-
   const onSubmit = () => history.push("/");
 
   return (
