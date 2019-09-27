b017262ea6bdbd5174c7248865bfa139ded98e2b
Nick Nance <nance.nick@gmail.com>
2019-09-27 07:09:06 -0700

    auth example with a context

---
+++ src/AuthContext.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export type FakeAuth = {
+    isAuthenticated: boolean
+    authenticate: (cb: () => void) => void
+    signout: (cb: () => void) => void
+}
+
+export const AuthContext = React.createContext<FakeAuth>({
+    isAuthenticated: false,
+    authenticate: () => {},
+    signout: () => {}
+  });
+
+export function useFakeAuth(): FakeAuth {
+    const [isAuthenticated, setIsAuthenticated] = React.useState(false);
+
+    const fakeAuth = (cb: () => void, result: boolean) => {
+        setTimeout(() => {
+            setIsAuthenticated(result);
+            cb();
+        }, 100);
+    }
+
+    return {
+        isAuthenticated,
+        authenticate: (cb: () => void) => {
+            fakeAuth(cb, true); // fake async
+        },
+        signout: (cb: () => void) => {
+            fakeAuth(cb, false);
+        }
+    }
+  };
+  
+  
--- src/AuthExample.tsx
+++ src/AuthExample.tsx
@@ -9,6 +9,8 @@ import {
   RouteComponentProps
 } from "react-router-dom";
 
+import { useFakeAuth, AuthContext } from "./AuthContext";
+
 ////////////////////////////////////////////////////////////
 // 1. Click the public page
 // 2. Click the protected page
@@ -16,66 +18,67 @@ import {
 // 4. Click the back button, note the URL each time
 
 function AuthExample() {
+  const auth = useFakeAuth();
   return (
-    <Router>
-      <div>
-        <AuthButton />
-        <ul>
-          <li>
-            <Link to="/public">Public Page</Link>
-          </li>
-          <li>
-            <Link to="/protected">Protected Page</Link>
-          </li>
-        </ul>
-        <Route path="/public" component={Public} />
-        <Route path="/login" component={Login} />
-        <PrivateRoute path="/protected" component={Protected}/>
-      </div>
-    </Router>
+    <AuthContext.Provider value={auth}>
+      <Router>
+        <div>
+          <AuthButton />
+          <ul>
+            <li>
+              <Link to="/public">Public Page</Link>
+            </li>
+            <li>
+              <Link to="/protected">Protected Page</Link>
+            </li>
+          </ul>
+          <Route path="/public" component={Public} />
+          <Route path="/login" component={Login} />
+          <PrivateRoute path="/protected" component={Protected}/>
+        </div>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
-const fakeAuth = {
-  isAuthenticated: false,
-  authenticate(cb: () => void) {
-    this.isAuthenticated = true;
-    setTimeout(cb, 100); // fake async
-  },
-  signout(cb: () => void) {
-    this.isAuthenticated = false;
-    setTimeout(cb, 100);
-  }
-};
-
 const AuthButton = withRouter(
-  ({ history }) =>
-    fakeAuth.isAuthenticated ? (
-      <p>
-        Welcome!{" "}
-        <button
-          onClick={() => {
-            fakeAuth.signout(() => history.push("/"));
-          }}
-        >
-          Sign out
-        </button>
-      </p>
-    ) : (
-      <p>You are not logged in.</p>
+  ({ history }) => {
+    return (
+      <AuthContext.Consumer>
+        { fakeAuth => fakeAuth.isAuthenticated ? (
+          <p>
+            Welcome!{" "}
+            <button
+              onClick={() => {
+                fakeAuth.signout(() => history.push("/"));
+              }}
+            >
+              Sign out
+            </button>
+          </p>
+          ) : (
+            <p>You are not logged in.</p>
+          )
+        }
+      </AuthContext.Consumer>
     )
+  }
 );
 
 const PrivateRoute = (props: RouteProps) => {
   const Component = props.component;
   return (
-    <Route path={props.path} render={routeProps => (
-      fakeAuth.isAuthenticated && Component ? (
-        <Component {...routeProps}/>
-      ) : (
-        <Redirect to={{ pathname: "/login", state: { from: routeProps.location } }}/>
-      )
-    )}/>
+    <AuthContext.Consumer>
+      { fakeAuth => (
+        <Route path={props.path} render={routeProps => (
+          fakeAuth.isAuthenticated && Component ? (
+            <Component {...routeProps}/>
+          ) : (
+            <Redirect to={{ pathname: "/login", state: { from: routeProps.location } }}/>
+          )
+        )}/>    
+      )}
+      </AuthContext.Consumer>
   )
 }
 
@@ -89,6 +92,7 @@ function Protected() {
 
 function Login(props: RouteComponentProps) {
   const [state, setState] = React.useState({ redirectToReferrer: false });
+  const fakeAuth = React.useContext(AuthContext);
 
   const login = () => {
     fakeAuth.authenticate(() => {
